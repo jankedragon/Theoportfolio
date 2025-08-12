2206ed2bcf3c3847432e3e93c3882a4ab299e465
jankedragon <redflame582@gmail.com>
2025-08-12 16:43:29 -0600

    fixed default name

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Fname Lname</title>
+        <title>Theodore Jolly</title>
       </head>
       <body>
         <Header />
